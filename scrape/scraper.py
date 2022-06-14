005858efdb6f52535b1c8b39ede3d38f7076ae1c
Lasse Benninga <devops.pipeline@example.com>
2022-06-14 20:34:37 +0200

    Don't accept gzip encoding

--- scrape/scraper.py
+++ scrape/scraper.py
@@ -14,14 +14,12 @@ HEADERS = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:95.0) Gecko/20100101 Firefox/95.0",
     "Accept": "application/json, text/plain, */*",
     "Accept-Language": "en-US,en;q=0.5",
-    "Accept-Encoding": "gzip, deflate, br",
     "Referer": "https://www.thuistreffervechtdal.nl/aanbod/te-huur",
     "X-Requested-With": "XMLHttpRequest",
     "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
     "Content-Length": "0",
     "Origin": "https://www.thuistreffervechtdal.nl",
     "Connection": "keep-alive",
-    # "Cookie": "__Host-PHPSESSID=e0869ad39f9fa841d848df83b846a8c8; __Host-PHPSESSID=4915e54bc48417ca0f255635f95e552d",
     "Sec-Fetch-Dest": "empty",
     "Sec-Fetch-Mode": "cors",
     "Sec-Fetch-Site": "same-origin",
@@ -30,7 +28,7 @@ HEADERS = {
 
 
 def scrape_listings():
-    response = requests.request("POST", URL, headers=HEADERS)
+    response = requests.request("GET", URL, headers=HEADERS)
 
     _res = json.loads(response.text)
 
